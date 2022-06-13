d7c92e61459ec9cfdd714d7240526c6be7401aac
Carmen1104 <carmencongko11@gmail.com>
2022-06-13 23:07:08 +0700

    Update MapGenerator.java

--- MapGenerator.java
+++ MapGenerator.java
@@ -1,4 +1,3 @@
-import java.nio.channels.FileChannel.MapMode;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
@@ -12,21 +11,23 @@ public class MapGenerator{
 
     public MapGenerator(int row, int col) {
        map = new int[row][col];
-       for (int []mapl : map) {
-           for (int i = 0; i < map[0].length; i++) {
-               mapl[i] = 1;
+       for (int i = 0; i < map.length; i++) {
+           for (int j = 0; j < map[0].length; j++) {
+               map[i][j] = 1;
            }
        } 
        brickWidth = 540/col;
-       brickHeight = 240/row;
+       brickHeight = 150/row;
     }
     
     public void draw(Graphics2D g) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 if(map[i][j] > 0) {
-                    g.setColor(Color.PINK);
+                    g.setColor(Color.white);
                     g.fillRect(j*brickWidth+80, i*brickHeight+50, brickWidth, brickHeight);
+
+                    // this is to show separate brick
                     g.setStroke(new BasicStroke(3));
                     g.setColor(Color.black);
                     g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
@@ -35,7 +36,7 @@ public class MapGenerator{
         }
     }
 
-    public void setBricksValue(int value,int row,int col)
+    public void setBrickValue(int value,int row,int col)
     {
         map[row][col] = value;
 

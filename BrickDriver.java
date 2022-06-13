ca1e0a0c8f91eef40b5329f10d433c75dbedb11b
Carmen1104 <carmencongko11@gmail.com>
2022-06-13 23:06:51 +0700

    Update BrickDriver.java

--- BrickDriver.java
+++ BrickDriver.java
@@ -8,12 +8,11 @@ public class BrickDriver {
         JFrame obj = new JFrame();
         Game gamePlay = new Game();
         obj.setBounds(10,10,700,600);
-        obj.setTitle("Brick Breaker Game");
+        obj.setTitle("Brick Smasher");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gamePlay);
-
     }
     
 }
